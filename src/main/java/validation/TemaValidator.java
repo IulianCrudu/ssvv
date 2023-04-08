456830e1929b09aa80601bb663cc4edb9db03e44
Iulian Crudu <cruduiulik@gmail.com>
2023-04-08 14:00:40 +0300

    Fix bug

--- src/main/java/validation/TemaValidator.java
+++ src/main/java/validation/TemaValidator.java
@@ -11,7 +11,8 @@ public class TemaValidator implements Validator<Tema> {
      */
     @Override
     public void validate(Tema entity) throws ValidationException {
-        if(entity.getID().equals("") || entity.getID() == null) {
+        if(entity.getID() == null || entity.getID().equals("")) {
+//        if(entity.getID().equals("") || entity.getID() == null) {
             throw new ValidationException("Numar tema invalid!");
         }
         if(entity.getDescriere().equals("")){
